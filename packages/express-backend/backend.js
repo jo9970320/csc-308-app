ddeb0d4d96f3b027a26e5cb707bf92acc4163550
Jose Ordonez <jo9970320@gmail.com>
2026-01-19 23:36:46 -0800

    Backend updates and frontend adjustments

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -37,7 +38,7 @@ const users = {
     
   ]
 };
-
+app.use(cors());
 app.use(express.json());
 
 const addUser = (user) => {
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,31 +1,35 @@
-import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
-
-
-
-  
+import React, { useState, useEffect } from "react";
 
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
-      function removeOneCharacter(index) {
-        const updated = characters.filter((character, i) => {
-          return i !== index;
-        });
-        setCharacters(updated);
-      }
-      return (
-        <div className="container">
-          <Table
-            characterData={characters}
-            removeCharacter={removeOneCharacter}
-          />
-          <Form handleSubmit={updateList} />
-  </div>
-      );
-      function updateList(person) {
-        setCharacters([...characters, person]);
-      }
-      
+  const [characters, setCharacters] = useState([]);
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
   }
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+}
 export default MyApp;

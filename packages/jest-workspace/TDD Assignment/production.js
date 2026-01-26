aeaadb6c892ee0eabacc5b1bdae02c3598c23ffa
Jose Ordonez <jo9970320@gmail.com>
2026-01-26 14:38:42 -0800

    2.2 Completed

--- packages/jest-workspace/TDD Assignment/production.js
+++ packages/jest-workspace/TDD Assignment/production.js
@@ -2,5 +2,8 @@ class StockPortfolio{
     constructor(){
         this.stocks = {};
     }
+    isEmpty(){
+        return Object.keys(this.stocks).length === 0;
+    }
 }
 module.exports = {StockPortfolio}
--- packages/jest-workspace/TDD Assignment/production.test.js
+++ packages/jest-workspace/TDD Assignment/production.test.js
@@ -6,3 +6,8 @@ test('2.1: Empty Portfolio -- success!', () => {
   const result = Object.keys(portf.stocks).length;;
   expect(target).toBe(result);
 });
+
+test("2.2: new portfolio is empty", () => {
+  const p = new StockPortfolio();
+  expect(p.isEmpty()).toBe(true);
+});

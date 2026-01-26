ec2db31e0804c436e54eb98e3cedb7634a68c40b
Jose Ordonez <jo9970320@gmail.com>
2026-01-26 11:43:53 -0800

    Made production code pass

--- packages/jest-workspace/TDD Assignment/tdd.js
+++ packages/jest-workspace/TDD Assignment/tdd.js
@@ -1,5 +1,11 @@
 function average_sum(array){
-    const total = 0;
+    let total = 0;
+    let size = array.length;
+    for (let i = 0; i < size; i++ ){
+        total += array[i];
+    }
+    const average = total / size;
+    return average
 }
 
 exports.average_sum = average_sum;
--- packages/jest-workspace/TDD Assignment/tdd.test.js
+++ packages/jest-workspace/TDD Assignment/tdd.test.js
@@ -1,7 +1,7 @@
 const functions = require("./tdd.js");
 
-test('Testing div -- success', () => {
+test('Testing average -- success', () => {
   const target = 5;
-  const result = functions.average_sum(2,4,6,8);
+  const result = functions.average_sum([2,4,6,8]);
   expect(target).toBe(result);
 });

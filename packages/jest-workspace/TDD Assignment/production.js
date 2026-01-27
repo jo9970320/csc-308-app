3c3ebe733b49347033c2129d426205ea83f35dfb
Jose Ordonez <jo9970320@gmail.com>
2026-01-26 19:11:28 -0800

    2.3 Completed... Adding shares passed

--- packages/jest-workspace/TDD Assignment/production.js
+++ packages/jest-workspace/TDD Assignment/production.js
@@ -5,5 +5,10 @@ class StockPortfolio{
     isEmpty(){
         return Object.keys(this.stocks).length === 0;
     }
+    purchase(symbol, shares) {
+        const current = this.stocks[symbol] ?? 0;
+        this.stocks[symbol] = current + shares;
+    }
+
 }
 module.exports = {StockPortfolio}
--- packages/jest-workspace/TDD Assignment/production.test.js
+++ packages/jest-workspace/TDD Assignment/production.test.js
@@ -11,3 +11,19 @@ test("2.2: new portfolio is empty", () => {
   const p = new StockPortfolio();
   expect(p.isEmpty()).toBe(true);
 });
+
+test('2.3: purchase adds shares to a new symbol!', () => {
+  const portf = new StockPortfolio(); 
+  portf.purchase("Item", 5) 
+  const target = portf.stocks["Item"];
+  const result = 5;
+  expect(target).toBe(result);
+});
+test("2.3: purchasing the same symbol twice adds shares", () => {
+  const portf = new StockPortfolio();
+  portf.purchase("Item", 5);
+  portf.purchase("Item", 2);
+  const target = portf.stocks["Item"];
+  const result = 7;
+  expect(target).toBe(result);
+});
